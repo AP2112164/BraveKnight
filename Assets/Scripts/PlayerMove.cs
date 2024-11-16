9a19a92d0f09872bf745a1787acc0fea93dc849b
adam <a.polec@rgu.ac.uk>
2024-11-16 22:32:38 +0000

    Another Commit
    More Stuff added to game

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,68 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public float speed;
+    public float rotationSpeed;
+    public float jumpForce;
+    private Rigidbody playerRB;
+    //public bool isOnGround = true;
+    public bool isOnSurfaces = true;
+
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal"); // player moves with A and D keys on x-axis
+        //float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, 0);
+
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+        // turns character to direction on key press
+        if (movementDirection != Vector3.zero)
+        {
+            animator.SetBool("isWalking", true);// plays walking animation while moving in both directions
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        } else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        if (Input.GetKeyDown("w") && isOnSurfaces)
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            //isOnGround = false;
+            isOnSurfaces = false;
+        }
+
+        if (Input.GetKeyDown("k")) // plays attacking animation
+        {
+            animator.SetBool("isAttacking", true);
+        }
+        else
+        {
+            animator.SetBool("isAttacking", false);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        //if(collision.gameObject.CompareTag("Ground"))
+        //{
+        //    isOnGround = true;
+        //}
+
+        if (collision.gameObject.CompareTag("Surface"))
+        {
+            isOnSurfaces = true;
+        }
     }
 }
